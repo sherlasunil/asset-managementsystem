03dd39aaf79bfa8ce572e923b60fd835f7194089
Sherla <Sherlas70@gmail.com>
2026-06-10 17:27:19 +0530

    Dynamic inventory page

--- frontend/src/pages/Inventory.js
+++ frontend/src/pages/Inventory.js
@@ -1,16 +1,46 @@
-import { inventory } from "../data/mockData";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Inventory() {
+  const [assets, setAssets] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://asset-management-system-2wz5.onrender.com/api/assets/",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((res) => setAssets(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h2>Inventory</h2>
-      <ul>
-        {inventory.map((item) => (
-          <li key={item.id}>
-            {item.item} - {item.quantity}
-          </li>
-        ))}
-      </ul>
+
+      <table className="table table-bordered">
+        <thead className="table-dark">
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {assets.map((asset) => (
+            <tr key={asset.id}>
+              <td>{asset.name}</td>
+              <td>{asset.type}</td>
+              <td>{asset.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
